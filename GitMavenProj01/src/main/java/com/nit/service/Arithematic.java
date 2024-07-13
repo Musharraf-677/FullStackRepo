c4d8f86a7422887fd8eddafee558419066e6a232
Shaikh Musharraf <Shaikh Musharraf@DESKTOP-NHK1E2R>
2024-07-13 15:05:16 +0530

    dev1-code

--- GitMavenProj01/src/main/java/com/nit/service/Arithematic.java
+++ GitMavenProj01/src/main/java/com/nit/service/Arithematic.java
@@ -1,5 +1,11 @@
 package com.nit.service;
 
-public class Arithematic {
+public class Arithematic 
+{
+	public int sum(int x,int y)
+	{
+		return x+y;
+		
+	}
 
 }
--- GitMavenProj01/src/test/java/com/nit/test/AppTest.java
+++ GitMavenProj01/src/test/java/com/nit/test/AppTest.java
@@ -1,5 +1,21 @@
 package com.nit.test;
 
-public class AppTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.nit.service.Arithematic;
+
+public class AppTest 
+{
+	@Test
+	public void testWithPositvies()
+	{
+		Arithematic ar = new Arithematic();
+		int expected = 50;
+		int actual = ar.sum(25,25);
+		assertEquals(expected, actual);
+		
+	}
 
 }
