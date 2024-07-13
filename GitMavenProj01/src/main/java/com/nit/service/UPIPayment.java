5fe66f0989208fbf629e435add818891145d5c84
Shaikh Musharraf <Shaikh Musharraf@DESKTOP-NHK1E2R>
2024-07-13 15:41:31 +0530

    TL-some improvement in branch2 code

--- GitMavenProj01/src/main/java/com/nit/service/UPIPayment.java
+++ GitMavenProj01/src/main/java/com/nit/service/UPIPayment.java
@@ -2,7 +2,7 @@ package com.nit.service;
 
 public class UPIPayment 
 {
-	public String GPay()
+	public String GPay(String userName,String password)
 	{
 		return "Payment done by googlePay Successfully...";
 		
